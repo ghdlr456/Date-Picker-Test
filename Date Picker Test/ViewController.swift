c0ccdca96d5e29190e38e5fbd4e8f9d556ff80fe
D7702_09 <d7702_09@d7702-09ui-imac.local>
2018-04-11 15:53:00 +0900

    Date Piker test

--- Date Picker Test/ViewController.swift
+++ Date Picker Test/ViewController.swift
@@ -3,17 +3,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let timeSelector: Selector = #selector(ViewController.update)
+    let interval = 1.0
+    var count = 0
+    
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var datepiker: UIDatePicker!
+    @IBOutlet weak var time2Label: UILabel!
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     }
 
-    @IBAction func DatePiker(_ sender: Any) {
+    @IBAction func DatePiker(_ sender: UIDatePicker) {
+        let datePikerView = sender
+        
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss a EE"
-        timeLabel.text = formatter.string(from: datepiker.date)
+        timeLabel.text = "선택시간: " + formatter.string(from: datePikerView.date)
+    }
     
+    @objc func update() {
+        let date = NSDate()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss a EE"
+        time2Label.text = "현재시간: " + formatter.string(from: date as Date)
     }
 }
